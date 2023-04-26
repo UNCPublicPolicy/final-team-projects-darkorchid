962d7d29db42888894b5b9b820494e4e1d86796f
paigesh <paigesh@live.unc.edu>
2023-04-26 09:50:31 -0400

    updates
    updates

--- DarkOrchid/Individual Work/Hathaway/Cleaned_data_Hathaway.R
+++
@@ -1,8 +0,0 @@
-#Final Project Data
-
-library(knitr)
-Hathaway_Data <- read.csv("/Users/paigehathaway/Documents/GitHub/final-team-projects-darkorchid/DarkOrchid/Hathaway/PolicyMap Data 2023-01-28 005957 UTC.csv")
-
-Hathaway_Data=subset(Hathaway_Data, select= -c(GeoID,Formatted.GeoID,Geographic.Vintage,Data.Source))
-
-setwd()
